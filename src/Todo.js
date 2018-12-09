79e5e85dbe1f03e30d239bacc99393ec5e28efba
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-09 16:00:27 +0100

    task can be added to database, but not showing on app wip

--- src/Todo.js
+++ src/Todo.js
@@ -2,26 +2,47 @@ import React from 'react'
 import { connect } from 'react-redux'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
+import { addTaskInputChangeAction, addNewTaskToDbAsyncAction } from './state/toDo'
+
+
 
 
 const ToDo = props => (
     <div>
         <TextField
-            hintText='Add task'
+            value={props._newTaskText}
+            onChange={props._addTaskInputChangeAction}
         />
         <RaisedButton
             label='Add task'
             primary={true}
+            onClick={props._addNewTaskToDbAsyncAction}
+
         />
         <TextField
             hintText='Find task'
         />
-
+        <RaisedButton
+            label='All tasks'
+            primary={true}
+        />
+        <RaisedButton
+            label='Uncompleted tasks'
+            primary={true}
+        />
+        <RaisedButton
+            label='Completed tasks'
+            primary={true}
+        />
 
     </div>
 )
 const mapStateToProps = state => ({
+    _newToDo: state.toDo.newToDo
+
 })
 const mapDispatchToProps = dispatch => ({
+    _addTaskInputChangeAction: (event) => dispatch(addTaskInputChangeAction(event.target.value)),
+    _addNewTaskToDbAsyncAction: () => dispatch(addNewTaskToDbAsyncAction())
 })
 export default connect(mapStateToProps, mapDispatchToProps)(ToDo) 
--- src/state/toDo.js
+++ src/state/toDo.js
@@ -1,12 +1,33 @@
+import { database } from '../firebaseConfig'
+const ADD_TASK_INPUT_CHANGE = 'toDo/ADD_TASK_INPUT_CHANGE'
+
 const INITIAL_STATE = {
     allToDos: [],
     visibleToDos: [],
     filterToDo: '',
     newToDo: ''
 }
- export default (state=INITIAL_STATE, action) => {
+
+
+export const addNewTaskToDbAsyncAction = () => (dispatch, getState) => {
+    const newTask = getState().toDo.newToDo
+    const uuid = getState().auth.user.uid
+    database.ref(`users/${uuid}/tasks`).push({
+        newTask
+    })
+}
+export const addTaskInputChangeAction = text => ({
+    type: ADD_TASK_INPUT_CHANGE,
+    text
+})
+export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case ADD_TASK_INPUT_CHANGE:
+            return {
+                ...state,
+                newToDo: action.text
+            }
         default:
-        return state
+            return state
     }
-} 
+}

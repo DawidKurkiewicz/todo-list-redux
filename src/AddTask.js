0b3058b38f05d4a39642469f315eacb3656a45b1
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2018-12-10 18:40:58 +0100

    comment AddTask.js since, im not using it yet

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import TextField from 'material-ui/TextField'
-import RaisedButton from 'material-ui/RaisedButton'
+// import React from 'react'
+// import TextField from 'material-ui/TextField'
+// import RaisedButton from 'material-ui/RaisedButton'
 
 
-const AddTask = props => (
-    <div>
-        <TextField
-            hintText='Add task'
-            value={props.OnChangeTextHandler}
-            onChange={props.OnTextHandler}
-        />
-        <RaisedButton
-            label='Add task'
-            primary={true}
-            onClick={props.OnClickHandler}
-        />
-    </div>
-)
+// const AddTask = props => (
+//     <div>
+//         <TextField
+//             hintText='Add task'
+//             value={props.OnChangeTextHandler}
+//             onChange={props.OnTextHandler}
+//         />
+//         <RaisedButton
+//             label='Add task'
+//             primary={true}
+//             onClick={props.OnClickHandler}
+//         />
+//     </div>
+// )
 
-export default AddTask
+// export default AddTask
